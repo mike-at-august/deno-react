a057aaeb00403d922c21a8aa8aa5bd918250baf0
Mike @ August <73315038+mike-at-august@users.noreply.github.com>
2021-04-22 21:15:30 -0400

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,15 +1,9 @@
-import HTML from "./components/html.js";
-
-export async function handler(req) {
+import { render } from './render.tsx'
+export async function handler() {
   return {
-    statusCode: 200,
     headers: {
-      "cache-control":
-        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
-      "content-type": "text/html; charset=utf8",
+      'content-type': 'text/html; charset=utf8'
     },
-    body: HTML({
-      title: "Deno Clock",
-    }),
-  };
+    body: await render()
+  }
 }
